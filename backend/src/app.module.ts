1f5e9c1a4ef5d4d6ef1286fd33976be1c80a65fb
mgtennant <100305096+mgtennant@users.noreply.github.com>
2022-05-11 12:31:38 -0700

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
+import { SitesModule } from "./sites/sites.module";
 
 console.log("Var check - POSTGRESQL_HOST", process.env.POSTGRESQL_HOST);
 console.log("Var check - POSTGRESQL_DATABASE", process.env.POSTGRESQL_DATABASE);
 console.log("Var check - POSTGRESQL_USER", process.env.POSTGRESQL_USER);
-if (process.env.POSTGRESQL_PASSWORD != null ){
+if (process.env.POSTGRESQL_PASSWORD != null) {
   console.log("Var check - POSTGRESQL_PASSWORD present");
 } else {
   console.log("Var check - POSTGRESQL_PASSWORD not present");
@@ -25,11 +25,11 @@ if (process.env.POSTGRESQL_PASSWORD != null ){
       database: process.env.POSTGRESQL_DATABASE || "postgres",
       username: process.env.POSTGRESQL_USER || "postgres",
       password: process.env.POSTGRESQL_PASSWORD,
-      // entities: [User],
+      // entities: [Site],
       autoLoadEntities: true, // Auto load all entities regiestered by typeorm forFeature method.
       synchronize: true, // This changes the DB schema to match changes to entities, which we might not want.
     }),
-    UsersModule,
+    SitesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
