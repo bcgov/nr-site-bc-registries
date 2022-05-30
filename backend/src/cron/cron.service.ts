de31f761aa729d053ea592e200bea1d9ade1c477
mgtennant <100305096+mgtennant@users.noreply.github.com>
2022-05-30 09:26:49 -0700

    Update cron.service.ts

--- backend/src/cron/cron.service.ts
+++ backend/src/cron/cron.service.ts
@@ -5,6 +5,8 @@ import * as csv from 'csvtojson';
 import axios from 'axios';
 import { aws4Interceptor } from 'aws4-axios';
 
+import * as fs from 'fs';
+
 import { SrassocsService } from '../srassocs/srassocs.service';
 import { SrdatesService } from '../srdate/srdates.service';
 import { SrdocparsService } from '../srdocpar/srdocpars.service';
@@ -38,25 +40,6 @@ import { SrsitparDto } from 'src/srsitpar/dto/srsitpar.dto';
 import { SrsiteDto } from 'src/srsites/dto/srsite.dto';
 import { SrdocparDto } from 'src/srdocpar/dto/srdocpar.dto';
 
-type RawDataObject = {
-  srassocs: string;
-  srdate: string;
-  srdocpar: string;
-  srevents: string;
-  srevpart: string;
-  srlands: string;
-  srparrol: string;
-  srpinpid: string;
-  srprfans: string;
-  srprfcat: string;
-  srprfque: string;
-  srprfuse: string;
-  srprofil: string;
-  srsitdoc: string;
-  srsites: string;
-  srsitpar: string;
-};
-
 @Injectable()
 export class CronService {
   constructor(
@@ -79,9 +62,11 @@ export class CronService {
   ) {}
 
   // @Cron('0,15,30,45 * * * * *')
-  @Cron('0,30 * * * * *')
+  @Cron('20 49 * * * *')
   async updateTables() {
     console.log('update tables job starting');
+    const timeTaken = 'Total time';
+    console.time(timeTaken);
     // grab data from bucket
     const data = await this.getData();
     // call removeall route on each Service
@@ -89,27 +74,42 @@ export class CronService {
     // call create on each json array entry
     await this.sendDataToTables(data);
     console.log('update tables job complete');
+    console.timeEnd(timeTaken);
   }
 
   // should fetch data from bucket, send to parseData function, and return the parsed data
   async getData() {
     // query the bucket
     let srassocs = await this.getCsv('srassocs.csv');
-    let srdate = await this.getCsv('srdate.csv');
-    let srdocpar = await this.getCsv('srdocpar.csv');
-    let srevents = await this.getCsv('srevents.csv');
-    let srevpart = await this.getCsv('srevpart.csv');
-    let srlands = await this.getCsv('srlands.csv');
-    let srparrol = await this.getCsv('srparrol.csv');
-    let srpinpid = await this.getCsv('srpinpid.csv');
-    let srprfans = await this.getCsv('srprfans.csv');
-    let srprfcat = await this.getCsv('srprfcat.csv');
-    let srprfque = await this.getCsv('srprfque.csv');
-    let srprfuse = await this.getCsv('srprfuse.csv');
+    // let srdate = await this.getCsv('srdate.csv');
+    let srdate = fs.readFileSync('./utils/srdate.csv');
+    // let srdocpar = await this.getCsv('srdocpar.csv');
+    let srdocpar = fs.readFileSync('./utils/srdocpar.csv');
+    // let srevents = await this.getCsv('srevents.csv');
+    let srevents = fs.readFileSync('./utils/srevents.csv');
+    // let srevpart = await this.getCsv('srevpart.csv');
+    let srevpart = fs.readFileSync('./utils/srevpart.csv');
+    // let srlands = await this.getCsv('srlands.csv');
+    let srlands = fs.readFileSync('./utils/srlands.csv');
+    // let srparrol = await this.getCsv('srparrol.csv');
+    let srparrol = fs.readFileSync('./utils/srparrol.csv');
+    // let srpinpid = await this.getCsv('srpinpid.csv');
+    let srpinpid = fs.readFileSync('./utils/srpinpid.csv');
+    // let srprfans = await this.getCsv('srprfans.csv');
+    let srprfans = fs.readFileSync('./utils/srprfans.csv');
+    // let srprfcat = await this.getCsv('srprfcat.csv');
+    let srprfcat = fs.readFileSync('./utils/srprfcat.csv');
+    // let srprfque = await this.getCsv('srprfque.csv');
+    let srprfque = fs.readFileSync('./utils/srprfque.csv');
+    // let srprfuse = await this.getCsv('srprfuse.csv');
+    let srprfuse = fs.readFileSync('./utils/srprfuse.csv');
     let srprofil = await this.getCsv('srprofil.csv');
-    let srsitdoc = await this.getCsv('srsitdoc.csv');
-    let srsites = await this.getCsv('srsites.csv');
-    let srsitpar = await this.getCsv('srsitpar.csv');
+    // let srsitdoc = await this.getCsv('srsitdoc.csv');
+    let srsitdoc = fs.readFileSync('./utils/srsitdoc.csv');
+    // let srsites = await this.getCsv('srsites.csv');
+    let srsites = fs.readFileSync('./utils/srsites.csv');
+    // let srsitpar = await this.getCsv('srsitpar.csv');
+    let srsitpar = fs.readFileSync('./utils/srsitpar.csv');
 
     let rawData = {
       srassocs: srassocs,
@@ -134,7 +134,7 @@ export class CronService {
   }
 
   // receives raw data and parses it
-  async parseData(rawData: RawDataObject) {
+  async parseData(rawData) {
     const parsedData = {
       srassocs: await csv().fromString(CSV_HEADERS.SRASSOCS + rawData.srassocs),
       srdate: await csv().fromString(CSV_HEADERS.SRDATE + rawData.srdate),
@@ -181,71 +181,140 @@ export class CronService {
   }
 
   async sendDataToTables(parsedData) {
-    const srassocs: [SrassocDto] = parsedData.srassocs;
-    for (const entry of srassocs) {
-      await this.srassocsService.create(entry);
-    }
+    console.log('adding data to tables...');
+    let counter = 0;
+    // const srassocs: [SrassocDto] = parsedData.srassocs;
+    // for (const entry of srassocs) {
+    //   await this.srassocsService.create(entry);
+    // }
     const srdate: [SrdateDto] = parsedData.srdate;
+    process.stdout.write(`Adding srdate   entry `);
     for (const entry of srdate) {
+      counter += 1;
+      process.stdout.write(`\rAdding srdate entry ${counter}/${srdate.length}`);
       await this.srdatesService.create(entry);
     }
     const srdocpar: [SrdocparDto] = parsedData.srdocpar;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srdocpar entry `);
     for (const entry of srdocpar) {
+      counter += 1;
+      process.stdout.write(`\rAdding srdocpar entry ${counter}/${srdocpar.length}`);
       await this.srdocparsService.create(entry);
     }
     const srevents: [SreventDto] = parsedData.srevents;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srevents entry `);
     for (const entry of srevents) {
+      counter += 1;
+      process.stdout.write(`\rAdding srevents entry ${counter}/${srevents.length}`);
       await this.sreventsService.create(entry);
     }
     const srevpart: [SrevpartDto] = parsedData.srevpart;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srevpart entry `);
     for (const entry of srevpart) {
+      counter += 1;
+      process.stdout.write(`\rAdding srevpart entry ${counter}/${srevpart.length}`);
       await this.srevpartsService.create(entry);
     }
     const srlands: [SrlandDto] = parsedData.srlands;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srlands entry `);
     for (const entry of srlands) {
+      counter += 1;
+      process.stdout.write(`\rAdding srlands  entry ${counter}/${srlands.length}`);
       await this.srlandsService.create(entry);
     }
     const srparrol: [SrparrolDto] = parsedData.srparrol;
+    counter = 0;
+    console.log('');
     for (const entry of srparrol) {
+      counter += 1;
+      process.stdout.write(`\rAdding srparrol entry ${counter}/${srparrol.length}`);
       await this.srparrolsService.create(entry);
     }
     const srpinpid: [SrpinpidDto] = parsedData.srpinpid;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srpinpid entry `);
     for (const entry of srpinpid) {
+      counter += 1;
+      process.stdout.write(`\rAdding srpinpid entry ${counter}/${srpinpid.length}`);
       await this.srpinpidsService.create(entry);
     }
     const srprfans: [SrprfanDto] = parsedData.srprfans;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srprfans entry `);
     for (const entry of srprfans) {
+      counter += 1;
+      process.stdout.write(`\rAdding srprfans entry ${counter}/${srprfans.length}`);
       await this.srprfansService.create(entry);
     }
     const srprfcat: [SrprfcatDto] = parsedData.srprfcat;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srprfcat entry `);
     for (const entry of srprfcat) {
+      counter += 1;
+      process.stdout.write(`\rAdding srprfcat entry ${counter}/${srprfcat.length}`);
       await this.srprfcatsService.create(entry);
     }
     const srprfque: [SrprfqueDto] = parsedData.srprfque;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srprfque entry `);
     for (const entry of srprfque) {
+      counter += 1;
+      process.stdout.write(`\rAdding srprfque entry ${counter}/${srprfque.length}`);
       await this.srprfquesService.create(entry);
     }
     const srprfuse: [SrprfuseDto] = parsedData.srprfuse;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srprfuse entry `);
     for (const entry of srprfuse) {
+      counter += 1;
+      process.stdout.write(`\rAdding srprfuse entry ${counter}/${srprfuse.length}`);
       await this.srprfusesService.create(entry);
     }
-    const srprofil: [SrprofilDto] = parsedData.srprofil;
-    for (const entry of srprofil) {
-      await this.srprofilsService.create(entry);
-    }
+    // const srprofil: [SrprofilDto] = parsedData.srprofil;
+    // for (const entry of srprofil) {
+    //   await this.srprofilsService.create(entry);
+    // }
     const srsitdoc: [SrsitdocDto] = parsedData.srsitdoc;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srsitdoc entry `);
     for (const entry of srsitdoc) {
+      counter += 1;
+      process.stdout.write(`\rAdding srsitdoc entry ${counter}/${srsitdoc.length}`);
       await this.srsitdocsService.create(entry);
     }
     const srsitpar: [SrsitparDto] = parsedData.srsitpar;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srsitpar entry `);
     for (const entry of srsitpar) {
+      counter += 1;
+      process.stdout.write(`\rAdding srsitpar entry ${counter}/${srsitpar.length}`);
       await this.srsitparsService.create(entry);
     }
     const srsites: [SrsiteDto] = parsedData.srsites;
+    counter = 0;
+    console.log('');
+    process.stdout.write(`Adding srsite entry `);
     for (const entry of srsites) {
+      counter += 1;
+      process.stdout.write(`\rAdding srsite  entry ${counter}/${srsites.length}`);
       await this.srsitesService.create(entry);
     }
-    console.log('added data to tables');
+    console.log('\nadded data to tables');
   }
 
   async getCsv(fileName: string) {
