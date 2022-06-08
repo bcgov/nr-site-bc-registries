40c75424011eff1649b45a97618c73342b2734d4
barrfalk <bfalk@salussystems.com>
2022-06-08 15:50:34 -0700

    Views included for prod

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,10 +7,10 @@ import * as hbs from 'hbs';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', '../public'));
-  app.setBaseViewsDir(join(__dirname, '..', '../views/pages'));
-  hbs.registerPartials(join(__dirname, '..', '../views/partials'));
-  hbs.registerPartials(join(__dirname, '..', '../views/layout'));
+  app.useStaticAssets(join(__dirname, '..', './public'));
+  app.setBaseViewsDir(join(__dirname, '..', './views/pages'));
+  hbs.registerPartials(join(__dirname, '..', './views/partials'));
+  hbs.registerPartials(join(__dirname, '..', './views/layout'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
