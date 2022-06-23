35cf7ce204c64848635f22cfab239bd4312a163b
mgtennant <100305096+mgtennant@users.noreply.github.com>
2022-06-23 12:00:53 -0700

    save info in session if report already paid for

--- frontend/src/bc-registry/bc-registry.controller.ts
+++ frontend/src/bc-registry/bc-registry.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Param, Controller, Header, Session, UseFilters, UseGuards } from '@nestjs/common';
+import { Get, Param, Controller, Header, Session, UseFilters, UseGuards, StreamableFile } from '@nestjs/common';
 import { AuthenticationFilter } from 'src/authentication/authentication.filter';
 import { AuthenticationGuard } from 'src/authentication/authentication.guard';
 import { PayService } from 'src/pay/pay.service';
@@ -18,14 +18,26 @@ export class BCRegistryController {
     @Param('reportType') reportType: string,
     @Param('siteId') siteId: string,
     @Session() session: { data?: SessionData }
-  ): Promise<any> {
-    return this.bcRegistryService.requestPdf(
-      reportType,
-      siteId,
-      session.data.name,
-      session.data.access_token,
-      session.data.account_id
-    );
+  ): Promise<StreamableFile | null> {
+    const isSaved = this.bcRegistryService.isReportSaved(siteId, reportType, session.data.savedReports);
+    let paymentStatus: string;
+    if (isSaved) {
+      return new StreamableFile(await this.bcRegistryService.getPdf(reportType, siteId, session.data.name));
+    } else {
+      if (reportType == 'synopsis') {
+        paymentStatus = await this.payService.createSynopsisInvoice(session.data.access_token, session.data.account_id);
+      } else if (reportType == 'detailed') {
+        paymentStatus = await this.payService.createDetailedInvoice(session.data.access_token, session.data.account_id);
+      } else {
+        return null; // report type error, payment api does not get called
+      }
+      if (paymentStatus == 'APPROVED' || paymentStatus == 'PAID' || paymentStatus == 'COMPLETED') {
+        session.data.savedReports.push([siteId, reportType]);
+        return new StreamableFile(await this.bcRegistryService.getPdf(reportType, siteId, session.data.name));
+      } else {
+        return null;
+      }
+    }
   }
 
   @Get('email-pdf/:reportType/:email/:siteId')
@@ -34,15 +46,30 @@ export class BCRegistryController {
     @Param('email') email: string,
     @Param('siteId') siteId: string,
     @Session() session: { data?: SessionData }
-  ): Promise<any> {
-    return this.bcRegistryService.requestEmail(
-      reportType,
-      decodeURI(email),
-      siteId,
-      session.data.name,
-      session.data.access_token,
-      session.data.account_id
-    );
+  ): Promise<{ message: string }> {
+    const isSaved = this.bcRegistryService.isReportSaved(siteId, reportType, session.data.savedReports);
+    let paymentStatus: string;
+    if (isSaved) {
+      return {
+        message: await this.bcRegistryService.emailHTML(reportType, email, siteId, session.data.name),
+      };
+    } else {
+      if (reportType == 'synopsis') {
+        paymentStatus = await this.payService.createSynopsisInvoice(session.data.access_token, session.data.account_id);
+      } else if (reportType == 'detailed') {
+        paymentStatus = await this.payService.createDetailedInvoice(session.data.access_token, session.data.account_id);
+      } else {
+        return { message: 'Report type error' };
+      }
+      if (paymentStatus == 'APPROVED' || paymentStatus == 'PAID' || paymentStatus == 'COMPLETED') {
+        session.data.savedReports.push([siteId, reportType]);
+        return {
+          message: await this.bcRegistryService.emailHTML(reportType, email, siteId, session.data.name),
+        };
+      } else {
+        return { message: 'Payment Error' };
+      }
+    }
   }
 
   @Get('nil-pdf/:searchType/:searchCriteria1/:searchCriteria2/:searchCriteria3')
@@ -54,13 +81,15 @@ export class BCRegistryController {
     @Param('searchCriteria2') searchCriteria2: string,
     @Param('searchCriteria3') searchCriteria3: string,
     @Session() session: { data?: SessionData }
-  ): Promise<any> {
-    return this.bcRegistryService.requestNilPdf(
-      searchType,
-      decodeURI(searchCriteria1),
-      decodeURI(searchCriteria2),
-      decodeURI(searchCriteria3),
-      session.data.name
+  ): Promise<StreamableFile> {
+    return new StreamableFile(
+      await this.bcRegistryService.requestNilPdf(
+        searchType,
+        decodeURI(searchCriteria1),
+        decodeURI(searchCriteria2),
+        decodeURI(searchCriteria3),
+        session.data.name
+      )
     );
   }
 }
