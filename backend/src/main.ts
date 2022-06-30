82fd904a169043f7fe7a06548ad393e86c27f281
barrfalk <bfalk@salussystems.com>
2022-06-30 15:43:15 -0700

    Environment Variables Updates

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   // local development backend port is 3001, docker backend port is 3000
-  const port = process.env.POSTGRESQL_HOST == 'database' ? 3000 : 3001;
+  const port = process.env.POSTGRESQL_HOST.includes('database') ? 3000 : 3001;
   await app.listen(port);
   const appService = app.get(AppService);
   await appService.initializeDb();
