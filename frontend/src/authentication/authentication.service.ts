0f2978ed11cbf696daa1c5083701c500adb54f60
barrfalk <bfalk@salussystems.com>
2022-07-04 15:07:58 -0700

    Update authentication.service.ts

--- frontend/src/authentication/authentication.service.ts
+++ frontend/src/authentication/authentication.service.ts
@@ -108,9 +108,27 @@ export class AuthenticationService {
       .then((res) => {
         return res.data;
       })
-      .catch((err) => {
-        console.log(err.response);
-        return err.response.data;
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("Response:");
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log("Request:");
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log("Error config:");
+        console.log(error.config);
+        console.log(error);
       });
   }
 
