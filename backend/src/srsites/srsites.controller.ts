b7395326bc02ffb3ee7edf1bc2b02860baef6839
mgtennant <100305096+mgtennant@users.noreply.github.com>
2023-06-28 15:40:23 -0700

    backend timestamps

--- backend/src/srsites/srsites.controller.ts
+++ backend/src/srsites/srsites.controller.ts
@@ -4,6 +4,7 @@ import { SrsitesService } from './srsites.service';
 import { CreateSrsiteDto } from './dto/create-srsite.dto';
 import { UpdateSrsiteDto } from './dto/update-srsite.dto';
 import { MinimalSiteData } from 'utils/constants';
+import { logCurrentTimePST } from 'utils/util';
 
 @ApiTags('srsites')
 @Controller('srsites')
@@ -17,36 +18,43 @@ export class SrsitesController {
 
   @Get()
   findAll() {
+    logCurrentTimePST('findAll');
     return this.srsitesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    logCurrentTimePST('findOne');
     return this.srsitesService.findOne(+id);
   }
 
   @Get('searchPid/:pid')
   searchPid(@Param('pid') pid: string): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchPid');
     return this.srsitesService.searchPid(pid);
   }
 
   @Get('searchCrownPin/:pin')
   searchCrownPin(@Param('pin') pin: string): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchCrownPin');
     return this.srsitesService.searchCrownPin(pin);
   }
 
   @Get('searchCrownFile/:crownLandsFileNumber')
   searchCrownFile(@Param('crownLandsFileNumber') crownLandsFileNumber: string): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchCrownFile');
     return this.srsitesService.searchCrownFile(decodeURIComponent(crownLandsFileNumber));
   }
 
   @Get('searchSiteId/:siteId')
   searchSiteId(@Param('siteId') siteId: string): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchSiteId');
     return this.srsitesService.searchSiteId(siteId);
   }
 
   @Post('searchAddress')
   searchAddress(@Body('address') address: string, @Body('city') city: string): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchAddress');
     return this.srsitesService.searchAddress(address, city);
   }
 
@@ -56,28 +64,33 @@ export class SrsitesController {
     @Param('lng') lng: string,
     @Param('size') size: string
   ): Promise<MinimalSiteData[]> | [] {
+    logCurrentTimePST('searchArea');
     return this.srsitesService.searchArea(lat, lng, size);
   }
 
   @Get('synopsisReport/:siteId')
   getSynopsisReportData(@Param('siteId') siteId: string) {
+    logCurrentTimePST('synopsisReport');
     return this.srsitesService.getSynopsisReportData(siteId);
   }
 
   @Get('detailedReport/:siteId')
   getDetailedReportData(@Param('siteId') siteId: string) {
+    logCurrentTimePST('detailedReport');
     return this.srsitesService.getDetailedReportData(siteId);
   }
 
   // id is unused, it's necessary for this route to work though
   @Get('getNilReportData/:id')
   getNilReportData() {
+    logCurrentTimePST('getNilReportData');
     return this.srsitesService.getNilReportData();
   }
 
   // only queries the db for the downloaddate, id is unused
   @Get('getSearchResultsData/:id')
   getSearchResultsData() {
+    logCurrentTimePST('getSearchResultsData');
     return this.srsitesService.getSearchResultsData();
   }
 
