3361882d468f6a4b68c272a2dcadfc169793dd5b
swezamm <91153866+swezamm@users.noreply.github.com>
2026-09-21 16:58:56 -0700

    Revert "the butons are not working throwing "Content Security Policy directive: "script-src 'self'""
    This reverts commit 568af694c9bc7326e206f9de9380a23942841c0a.

--- frontend/public/js/util.js
+++ frontend/public/js/util.js
@@ -1,8 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-$(function () {
-  $('[data-toggle="popover"]').popover();
-});
-
 // utility functions
 function back() {
   const urlPath = window.location.pathname;
@@ -834,12 +830,6 @@ function setFolio(folioInput) {
 
 var folioInput = document.getElementById('folioInput');
 folioInput.addEventListener('input', setFolio(folioInput));
-
-// Shared nav/action button wiring (present on some, not all, pages)
-document.getElementById('backButton')?.addEventListener('click', back);
-document.getElementById('goToIndexButton')?.addEventListener('click', goToIndex);
-document.getElementById('goToDashboardButton')?.addEventListener('click', goToDashboard);
-document.getElementById('goToPrevPageButton')?.addEventListener('click', goToPrevPage);
 
 window.onload = function () {
   const folioValue = localStorage.getItem('folio');
--- frontend/src/site-registry/pipe/area-search.pipe.ts
+++ frontend/src/site-registry/pipe/area-search.pipe.ts
@@ -3,9 +3,9 @@ import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from
 @Injectable()
 export class AreaSearchPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    const lat = value.lat;
-    const lng = value.lng;
-    const size = value.size;
+    let lat = value.lat;
+    let lng = value.lng;
+    let size = value.size;
 
     if (Number.isNaN(parseFloat(lat))) {
       throw new BadRequestException('Latitude is not a number');
